8658a2d8ec84a4dd048bdfd5ffc1554913347fd9
Akshay-2203 <bhaardwajakshu629@gmail.com>
2024-07-03 14:40:17 +0530

    Commiting Pattern related codes

---
+++ Pattern01Trangle.java
@@ -0,0 +1,23 @@
+package learning.java;
+
+public class Pattern01Trangle {
+	public static void main(String[]args) {
+		int n =5;
+//		outer loop
+		for(int i=1;i<=n;i++) {
+//			inner loop
+			for(int j=1;j<=i;j++) {
+				int sum =i+j;
+				if(sum%2==0) {
+//					even	
+					System.out.print(" 1"); 
+					
+				}else System.out.print(" 0");
+//				odd
+			}
+			System.out.println();
+			
+		}
+	}
+
+}
